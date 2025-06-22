d4a8c7f87194c5871a8c6f6885aa20ba7c00e999
Manuel <manuelbustamantealiaga@gmail.com>
2025-06-22 14:05:08 -0400

    nuevo commit

--- src/main/java/cl/duoc/ferremas/Controller/ProductoController.java
+++ src/main/java/cl/duoc/ferremas/Controller/ProductoController.java
@@ -15,21 +15,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 @RestController
 
 
